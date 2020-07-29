98a08a136c4f455139abb61fb6b66cd972aba4d9
nshorty <andrysciortino@gmail.com>
2020-07-29 14:07:08 +0200

    MSM-Xtended bringup!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_tulip.mk
+    $(LOCAL_DIR)/xtended_tulip.mk
 
 COMMON_LUNCH_CHOICES := \
-   aosp_tulip-user \
-   aosp_tulip-userdebug \
-   aosp_tulip-eng
+   xtended_tulip-user \
+   xtended_tulip-userdebug \
+   xtended_tulip-eng
